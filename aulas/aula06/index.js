92a698e53832d741ff2d8a5e7d0a93fa29163229
CaioAleixo <caio.aleixo@iesb.edu.br>
2025-09-11 20:36:50 -0300

    Adicionando pratica04

--- aulas/aula06/index.js
+++ aulas/aula06/index.js
@@ -1,30 +1,26 @@
 // importa o framework 
 const express = require("express");
 
+// importa middleware de terceiros
+const cors = require('cors');
+
+// importa middleware de rota
+const router = require('./routerTarefa');
+
 // criar uma instância da aplicação
 const app = express();
+
+// middleware embutido ou integrado
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// middleware de terceiro
+app.use(cors());
 
 // middleware de aplicação
 app.use( function (req, res, next) {
     console.log("Passei aqui!");
     next();
-});
-
-// middleware de rota
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send("Chegou aqui!");
-});
-
-router.post('/', (req, res) => {
-    res.status(201).send("Inserindo com sucesso!");
-});
-
-router.get("/:id", (req, res) => {
-    const { id } = req.params;  // {id: 1, param2: 5, param3: 6}
-    if(id == 1) res.send("Achei");
-    else res.status(404).send("Não achei");
 });
 
 app.use('/tarefas', router);

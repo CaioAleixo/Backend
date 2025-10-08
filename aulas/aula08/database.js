9b058944a0bcf93b7921181f87ab46cc118a5a3f
CaioAleixo <caio.aleixo@iesb.edu.br>
2025-10-07 21:47:40 -0300

    Aula 08

---
+++ aulas/aula08/database.js
@@ -0,0 +1,20 @@
+// objeto cliente do mongodb
+const { MongoClient } = require("mongodb");
+
+// string de conexão
+const url = "mongodb+srv://usrTarefas:caio1234@caioaleixo.fwwapkh.mongodb.net/";
+
+const client = new MongoClient(url);
+ 
+async function conecta() {
+    try {
+        await client.connect();
+        return client.db("agenda");
+    }
+    
+    catch (e) {
+        console.log("Erro ao conectar no MongoDB", e.message);
+    }
+}
+
+module.exports = conecta;
